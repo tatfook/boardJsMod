df4ab102d4745670cebab3b99d06f26c2d316636
onedou <onedou@126.com>
2017-10-09 11:36:15 +0800

    Initialized

---
+++ Gruntfile.js
@@ -0,0 +1,31 @@
+module.exports = function(grunt) {
+	grunt.initConfig({
+		"pkg"     : grunt.file.readJSON('package.json'),
+		"concat"  : {
+			"options" : {
+				"separator" : ";"
+			},
+			"dist"    : {
+				"src"  : ['js/Init.js'     , 'jscolor/jscolor.js' , 'sanitizer/sanitizer.min.js' , 'js/mxClient.js' ,
+						  'js/EditorUI.js' , 'js/Editor.js'       , 'js/Sidebar.js'              , 'js/Graph.js'    ,
+						  'js/Shapes.js'   , 'js/Actions.js'      , 'js/Menus.js'                , 'js/Format.js'   ,
+						  'js/Toolbar.js'  , 'js/Dialogs.js'      ,],
+				"dest" : "dist/<%= pkg.name %>.js",
+			}
+		},
+		"uglify" : {
+			"options" : {
+				"banner" : "/* <%= pkg.name %> <%= grunt.template.today('yyyy-mm-dd') %> */"
+			},
+			"build"   : {
+				"src"  : "dist/<%= pkg.name %>.js",
+				"dest" : "dist/<%= pkg.name %>.min.js"
+			}
+		}
+	});
+
+	grunt.loadNpmTasks('grunt-contrib-uglify');
+	grunt.loadNpmTasks('grunt-contrib-concat');
+
+	grunt.registerTask('default', ['concat', 'uglify']);
+}
